52dddabfc95e5a8a76d551ff4f60bc92c16dcd11
Adrian Shkumbov <adrian.shkumbov@gmail.com>
2026-07-06 21:08:34 -0400

    Finished thesis page & removed redundant experience page, moving relevant info to home. Stylistic changes all around.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://AdrianShkumbov.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
 });
